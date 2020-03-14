679709df99ae0bf5b690b4e57f8d39e2a2387f29
Thomas Schagerl <thomas.schagerl@gmx.net>
2020-03-14 16:21:21 +0100

    [FEATURE] add ajax submit to form

--- wp-content/plugins/joinpoints/joinpoints.php
+++ wp-content/plugins/joinpoints/joinpoints.php
@@ -8,23 +8,53 @@
  * Author URI: https://socket.games/
  */
 
-defined('ABSPATH') or die('No script kiddies please!');
+defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
 
-function joinpoints_form($args = [], $content = null) {
-    wp_enqueue_script(
-        'joinpoints',
-        plugins_url('/public/js/joinpoints.js', __FILE__),
-        [],
-        FALSE,
-        TRUE
-    );
-    $content = '
-        <form method="post">
-            Hello Joinpoints!
-            TODO : add form here
-            <button type="submit">Absenden</button>
+function joinpoints_form( $args = [], $content = null ) {
+	wp_enqueue_script(
+		'joinpoints',
+		plugins_url( '/public/js/joinpoints.js', __FILE__ ),
+		[ 'jquery' ],
+		false,
+		true
+	);
+	wp_localize_script(
+		'joinpoints',
+		'joinpoints',
+		[ 'ajax_url' => admin_url( 'admin-ajax.php' ) ]
+	);
+	$content = '
+        <form
+        	method="post"
+        	class="joinpoints-form"
+        >
+        	<input
+	            type="text"
+	            name="test"
+	            placeholder="Test Value"
+	            required
+        	>
+            <button
+            	type="submit"
+            	class="joinpoints-form__submit"
+            	disabled
+            >
+            	Absenden
+            </button>
         </form>
     ';
-    return $content;
+
+	return $content;
 }
-add_shortcode('joinpoints_form', 'joinpoints_form');
+
+function joinpoints_ajax_handler( $args ) {
+	wp_send_json([
+		'status' => 'success',
+		'message' => "hello {$_POST['data']}",
+	]);
+	wp_die();
+}
+
+add_action( 'wp_ajax_nopriv_jointpoins-form__submit', 'joinpoints_ajax_handler' );
+
+add_shortcode( 'joinpoints_form', 'joinpoints_form' );
--- wp-content/plugins/joinpoints/public/js/joinpoints.js
+++ wp-content/plugins/joinpoints/public/js/joinpoints.js
@@ -1,1 +1,19 @@
-console.log('hello world this is joinpoints');
+jQuery(($) => {
+    $('.joinpoints-form__submit').removeAttr('disabled');
+    $('.joinpoints-form').on('submit', async (e) => {
+        e.preventDefault();
+        const { ajax_url } = window.joinpoints;
+        const data = {
+            action: 'jointpoins-form__submit',
+            data: $(e.currentTarget).find('[name=test]').val(),
+        };
+        try {
+            const response = await $.post(ajax_url, data);
+            // TODO : show success page to user
+            console.log('success', response);
+        } catch (e) {
+            // TODO : show error message to user
+            console.log('unable to send', e);
+        }
+    });
+});
