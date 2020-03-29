daefc75d457fbd3503de630a6f6f5e69fb44b676
Thomas Schagerl <thomas.schagerl@gmx.net>
2020-03-29 11:20:26 +0200

    [BUGFIX] joinpoints auth

--- wp-content/plugins/joinpoints/joinpoints.php
+++ wp-content/plugins/joinpoints/joinpoints.php
@@ -10,6 +10,7 @@
 
 defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
 
+// region options page
 function joinpoints_plugin_options_init() {
 	register_setting( 'joinpoints_api_auth', 'joinpoints_api_auth' );
 	add_settings_section(
@@ -46,17 +47,17 @@ function joinpoints_plugin_section_api() {
 }
 
 function joinpoints_plugin_setting_app_id() {
-	$options = get_option('joinpoints_api_auth');
+	$options = get_option( 'joinpoints_api_auth' );
 	echo "<input id='joinpoints_app_id' name='joinpoints_api_auth[app_id]' size='32' type='text' value='{$options['app_id']}' />";
 }
 
 function joinpoints_plugin_setting_access_key() {
-	$options = get_option('joinpoints_api_auth');
+	$options = get_option( 'joinpoints_api_auth' );
 	echo "<input id='joinpoints_access_key' name='joinpoints_api_auth[access_key]' size='64' type='text' value='{$options['access_key']}' />";
 }
 
 function joinpoints_plugin_setting_secret() {
-	$options = get_option('joinpoints_api_auth');
+	$options = get_option( 'joinpoints_api_auth' );
 	echo "<input id='joinpoints_secret' name='joinpoints_api_auth[secret]' size='24' type='text' value='{$options['secret']}' />";
 }
 
@@ -75,14 +76,14 @@ function joinpoints_plugin_options_page() {
 		wp_die( __( 'You do not have sufficient permissions to access this page.' ) );
 	}
 	?>
-		<div class="wrap">
-			<h1>Joinpoints Settings</h1>
-			<form method="post" action="options.php">
-				<?php settings_fields('joinpoints_api_auth'); ?>
-				<?php do_settings_sections('joinpoints'); ?>
-				<?php submit_button(); ?>
-			</form>
-		</div>
+    <div class="wrap">
+        <h1>Joinpoints Settings</h1>
+        <form method="post" action="options.php">
+			<?php settings_fields( 'joinpoints_api_auth' ); ?>
+			<?php do_settings_sections( 'joinpoints' ); ?>
+			<?php submit_button(); ?>
+        </form>
+    </div>
 	<?php
 }
 
@@ -90,7 +91,9 @@ if ( is_admin() ) {
 	add_action( 'admin_menu', 'joinpoints_plugin_menu' );
 	add_action( 'admin_init', 'joinpoints_plugin_options_init' );
 }
+// endregion options page
 
+// region form
 function joinpoints_form( $args = [], $content = null ) {
 	wp_enqueue_script(
 		'joinpoints',
@@ -128,32 +131,65 @@ function joinpoints_form( $args = [], $content = null ) {
 	return $content;
 }
 
-function joinpoints_ajax_handler( $args ) {
-	$options = get_option('joinpoints_api_auth');
-	$ch  = curl_init();
-	$url = 'https://www.joinpoints.net/api-v1/oauth/auth';
-	$url .= '?app_id=' . $options['app_id'];
-	$url .= '&grant_type=implicit';
-	$url .= '&access_key=' . $options['access_key'];
-	$url .= '&secret=' . $options['secret'];
+add_shortcode( 'joinpoints_form', 'joinpoints_form' );
+// endregion form
+
+// region ajax handler
+function joinpoints_get_access_token( &$errorMessage = 'Joinpoints API unbekannter Fehler.' ) {
+	$options = get_option( 'joinpoints_api_auth' );
+	$ch      = curl_init();
+	$url     = 'https://www.joinpoints.net/api/oauth/auth';
+	$url     .= '?app_id=' . $options['app_id'];
+	$url     .= '&grant_type=implicit';
+	$url     .= '&access_key=' . $options['access_key'];
+	$url     .= '&secret=' . $options['secret'];
 	curl_setopt( $ch, CURLOPT_URL, $url );
 	curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true );
 	$response = json_decode( curl_exec( $ch ) );
 	$httpCode = curl_getinfo( $ch, CURLINFO_HTTP_CODE );
 	curl_close( $ch );
 
+	if ( $httpCode !== 200 ) {
+		$errorMessage = 'Joinpoints API nicht erreichbar.';
+
+		return null;
+	}
+
+	if ( empty( $response->data->access_token ) ) {
+		if (
+			! empty( $response->status )
+			&& $response->status === 'error'
+			&& ! empty( $response->status_description )
+		) {
+			$errorMessage = 'Joinpoints API Fehler: ' . $response->status_description;
+		} else {
+			$errorMessage = 'Joinpoints API unbekannter Fehler.';
+		}
+
+		return null;
+	}
+
+	return $response->data->access_token;
+}
+
+function joinpoints_ajax_handler__form_submit( $args ) {
+	$accessToken = joinpoints_get_access_token( $errorMessage );
+	if ( empty( $accessToken ) ) {
+		wp_send_json( [
+			'status'  => 'error',
+			'message' => $errorMessage,
+		] );
+		wp_die();
+	}
+
 	wp_send_json( [
 		'status'   => 'success',
 		'message'  => "hello {$_POST['data']}",
-		'response' => [
-			'head' => $response,
-			'code' => $httpCode,
-		],
 	] );
 	wp_die();
 }
 
-add_action( 'wp_ajax_nopriv_jointpoins-form__submit', 'joinpoints_ajax_handler' );
-add_action( 'wp_ajax_jointpoins-form__submit', 'joinpoints_ajax_handler' );
+add_action( 'wp_ajax_nopriv_jointpoins-form__submit', 'joinpoints_ajax_handler__form_submit' );
+add_action( 'wp_ajax_jointpoins-form__submit', 'joinpoints_ajax_handler__form_submit' );
+// endregion ajax handler
 
-add_shortcode( 'joinpoints_form', 'joinpoints_form' );
